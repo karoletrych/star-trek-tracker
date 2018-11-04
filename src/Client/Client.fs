6b387c8ef6315c4d5655fefc1939bad762d4dd23
Karol Etrych <karol.etrych@gmail.com>
2018-11-04 10:16:17 +0100

    implement quickview react component

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -6,6 +6,7 @@ open Elmish.React
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 open Fable.PowerPack.Fetch
+open Fable.Import
 
 open Thoth.Json
 
@@ -91,16 +92,42 @@ let tile =
            ]
 let hide e =
     ()
-let quickview = 
-    Quickview.quickview [ Quickview.IsActive true ]
-                        [ Quickview.header [ ]
-                            [ Quickview.title [ ] [ str "Testing..." ]
-                              Delete.delete [ Delete.OnClick hide ] [ ] ]
-                          Quickview.body [ ]
-                            [ p [ ] [ str "The body" ] ]
-                          Quickview.footer [ ]
-                            [ Button.button [ Button.OnClick hide ]
-                                            [ str "Hide the quickview!" ] ] ]
+
+type QuickviewDemoProps =
+    interface end
+
+type QuickviewDemoState =
+    { IsActive : bool }
+
+
+type QuickviewDemo(props) =
+    inherit React.Component<QuickviewDemoProps, QuickviewDemoState>(props)
+    do base.setInitState({ IsActive = false })
+
+    member this.show _ =
+        { this.state with
+                        IsActive = true }
+        |> this.setState
+
+    member this.hide _ =
+        { this.state with
+                        IsActive = false }
+        |> this.setState
+
+    override this.render () =
+        div [ ]
+            [ Quickview.quickview [ Quickview.IsActive this.state.IsActive ]
+                    [ Quickview.header [ ]
+                        [ Quickview.title [ ] [ str "Testing..." ]
+                          Delete.delete [ Delete.OnClick this.hide ] [ ] ]
+                      Quickview.body [ ]
+                        [ p [ ] [ str "The body" ] ]
+                      Quickview.footer [ ]
+                        [ Button.button [ Button.OnClick this.hide ]
+                                        [ str "Hide the quickview!" ] ] ]
+              Button.button [ Button.Color IsPrimary
+                              Button.OnClick this.show ]
+                            [ str "Show the Quickview!" ] ]
 
 let view (model : Model) (dispatch : Msg -> unit) =
     div []
@@ -109,8 +136,8 @@ let view (model : Model) (dispatch : Msg -> unit) =
                 [ Heading.h2 [ ]
                     [ str "SAFE Template" ] ] ]
 
+          ofType<QuickviewDemo,_,_> (unbox null) []
           tile
-          quickview
           Footer.footer [ ]
                 [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
                     [ safeComponents ] ] ]
